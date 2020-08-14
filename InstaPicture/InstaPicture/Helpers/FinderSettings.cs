8c441fbc59d6e3636daf7d91440818579eb77c08
TheUnforg1ven <tbatjuk4u@gmail.com>
2020-08-14 17:24:32 +0300

    Changed gitignore

--- InstaPicture/InstaPicture/Helpers/FinderSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InstaPicture.Helpers
-{
-	public static class FinderSettings
-	{
-		public static string Username { get; set; } = "<username>";
-
-		public static string Password { get; set; } = "<password>";
-	}
-}
